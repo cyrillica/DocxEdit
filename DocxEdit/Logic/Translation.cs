12c84fc9f709753f23d4014ff6e1c48cc254f45f
izen57 <golovoslon@gmail.com>
2022-12-11 21:03:40 +0300

    DocxEdit plugin

---
+++ DocxEdit/Logic/Translation.cs
@@ -0,0 +1,89 @@
+﻿using Aspose.Words;
+using Aspose.Words.Tables;
+
+using System.Collections;
+using System.Text.RegularExpressions;
+
+namespace SubtitleEdit.Logic
+{
+	public static class Translation
+	{
+		public static void DocxToTxt(string file_name)
+		{
+			var document = new Document(file_name);
+			DocumentBuilder builder = new DocumentBuilder(document);
+			var newFileName = Regex.Replace(file_name, @"\.doc.$", ".txt");
+
+			Node[] tables = document.GetChildNodes(NodeType.Table, true).ToArray();
+			foreach (Table table in tables)
+			{
+				Paragraph par = new Paragraph(document);
+
+				table.ParentNode.InsertAfter(par, table);
+				builder.MoveTo(par);
+				builder.Font.Name = "Courier New";
+
+				builder.Writeln(ConvertTable(table));
+				table.Remove();
+			}
+			document.Save(newFileName, SaveFormat.Text);
+		}
+
+		private static string ConvertTable(Table tab)
+		{
+			string output = string.Empty;
+
+			ArrayList columnWidhs = new ArrayList();
+			int tableWidth = 0;
+			string horizontalBorder = string.Empty;
+
+			foreach (Row row in tab.Rows)
+				foreach (Cell cell in row.Cells)
+				{
+					int cellIndex = row.Cells.IndexOf(cell);
+					if (columnWidhs.Count > cellIndex)
+					{
+						if ((int) columnWidhs[cellIndex] < cell.GetText().Length)
+							columnWidhs[cellIndex] = cell.GetText().Length;
+					}
+					else
+						columnWidhs.Add(cell.GetText().Length);
+				}
+
+			//Calculate width of table
+			for (int index = 0; index < columnWidhs.Count; index++)
+				tableWidth += (int) columnWidhs[index];
+			tableWidth += columnWidhs.Count;
+
+			//for (int index = 0; index < tableWidth; index++)
+			//	horizontalBorder += "-";
+			//horizontalBorder += "\r\n";
+
+			output += horizontalBorder;
+
+			NodeCollection tableNotes = tab.GetChildNodes(NodeType.Paragraph, true);
+			foreach (Row row in tab.Rows)
+			{
+				string currentRow = string.Empty;
+
+				foreach (Cell cell in row.Cells)
+				{
+					int cellIndex = row.Cells.IndexOf(cell);
+
+					string curentCell = cell.GetText().Replace("\a", "").Replace("\n", "").Replace("\r", "");
+
+					//Insert white spaces to the end of cell text
+					//while (curentCell.Length < (int) columnWidhs[cellIndex])
+					//	curentCell += " ";
+
+					if (cellIndex != row.Cells.Count - 1)
+						curentCell += "\t";
+					currentRow += curentCell;
+				}
+				output += currentRow + "\r\n";
+			}
+
+			return output;
+		}
+	}
+}
---
+++ DocxEdit/Plugin.cs
@@ -0,0 +1,56 @@
+﻿using SubtitleEdit;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace Nikse.SubtitleEdit.PluginLogic
+{
+	public class DocxEdit : IPlugin // dll file name must "<classname>.dll" - e.g. "Haxor.dll"
+	{
+		string IPlugin.Name => "DocxEdit";
+
+		string IPlugin.Text => "DocxEdit (открыть файл)";
+
+		decimal IPlugin.Version => 0.1M;
+
+		string IPlugin.Description => "Edit MS Word files";
+
+		// Can be one of these: file, tool, sync, translate, spellcheck
+		string IPlugin.ActionType => "file";
+
+		string IPlugin.Shortcut => string.Empty;
+
+		string IPlugin.DoAction(Form parentForm, string subtitle, double frameRate, string listViewLineSeparatorString, string subtitleFileName, string videoFileName, string rawText)
+		{
+			OpenFileDialog openFileDialog = new OpenFileDialog
+			{
+				Filter = "Файлы MS Word (*.docx; *.doc)|*.docx; *.doc"
+			};
+			openFileDialog.FileName
+			//subtitle = subtitle.Trim();
+			//if (string.IsNullOrEmpty(subtitle))
+			//{
+			//	MessageBox.Show("Субтитры не загружены", parentForm.Text,
+			//		MessageBoxButtons.OK, MessageBoxIcon.Warning);
+			//	return string.Empty;
+			//}
+
+			if (!string.IsNullOrEmpty(listViewLineSeparatorString))
+				Configuration.ListViewLineSeparatorString = listViewLineSeparatorString;
+
+			var list = new List<string>();
+			foreach (string line in subtitle.Replace(Environment.NewLine, "\n").Split('\n'))
+				list.Add(line);
+
+			var sub = new Subtitle();
+			var srt = new SubRip();
+			srt.LoadSubtitle(sub, list, subtitleFileName);
+			using (var form = new MainForm(sub, (this as IPlugin).Text, (this as IPlugin).Description, parentForm))
+			{
+				if (form.ShowDialog(parentForm) == DialogResult.OK)
+					return form.FixedSubtitle;
+			}
+			return string.Empty;
+		}
+	}
+}
